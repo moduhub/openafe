b9fd4ac1c10f7d85802085f5822a3fe1ecfe084c
Eduardo-Paz-Putti <eduardopazputti@gmail.com>
2025-08-13 17:59:32 -0300

    [add] Added JSDoc for the Arduino platform functions

--- src/platform/arduino/platform_arduino.hpp
+++ src/platform/arduino/platform_arduino.hpp
@@ -14,14 +14,69 @@ extern "C" {
 // D12 - MISO - PB4
 // D13 - SCK  - PB5
 
-void arduino_spi_begin(uint8_t pShieldCSPin, uint32_t pSPIClockSpeed);
-uint8_t arduino_spi_transfer(uint8_t pByte);
-void arduino_CSLow(void);
-void arduino_CSHigh(void);
+ /**
+  * @brief Initialize the Arduino platform.
+  *
+  * @param pShieldCSPin The chip select pin for the shield.
+  * @param pShieldResetPin The reset pin for the shield.
+  * @param pSPIClockSpeed The SPI clock speed.
+  */
 void arduino_setup(uint8_t pShieldCSPin, uint8_t pShieldResetPin, uint32_t pSPIClockSpeed);
+
+/** 
+  * @brief Begin SPI communication.
+  *
+  * @param pShieldCSPin The chip select pin for the shield.
+  * @param pSPIClockSpeed The SPI clock speed.
+  */
+void arduino_spi_begin(uint8_t pShieldCSPin, uint32_t pSPIClockSpeed);
+
+/** 
+  * @brief Transfer a byte over SPI.
+  *
+  * @param pByte The byte to transfer.
+  * @return The byte received.
+  */
+uint8_t arduino_spi_transfer(uint8_t pByte);
+
+/** 
+  * @brief Set the chip select pin low.
+  */
+void arduino_CSLow(void);
+
+/** 
+  * @brief Set the chip select pin high.
+  */
+void arduino_CSHigh(void);
+
+/** 
+  * @brief Delay for a specified number of microseconds.
+  *
+  * @param pDelay_us The delay duration in microseconds.
+  */
 void arduino_delayMicroseconds(uint64_t pDelay_us);
+
+/** 
+  * @brief Reset the Arduino platform.
+  */
 void arduino_reset(void);
+
+/** 
+  * @brief Read data from the SPI bus.
+  *
+  * @param pRXBuffer The buffer to store the received data.
+  * @param pBufferSize The size of the buffer.
+  * @return The number of bytes read.
+  */
 uint8_t arduino_SPIRead(uint8_t *pRXBuffer, uint8_t pBufferSize);
+
+/** 
+  * @brief Write data to the SPI bus.
+  *
+  * @param pTXBuffer The buffer containing the data to send.
+  * @param pBufferSize The size of the buffer.
+  * @return The number of bytes written.
+  */
 uint8_t arduino_SPIWrite(uint8_t *pTXBuffer, uint8_t pBufferSize);
 
 #ifdef __cplusplus
