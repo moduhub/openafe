410a0c475d749c910e23cd5daf24c3d675fd279a
Eduardo-Paz-Putti <eduardopazputti@gmail.com>
2025-08-20 09:33:29 -0300

    [doc] Documented as the functions of the AVR

--- src/platform/avr/platform_avr.h
+++ src/platform/avr/platform_avr.h
@@ -2,10 +2,6 @@
 extern "C" {
 #endif // __cplusplus
 
-#ifndef ARDUINO_ARCH_AVR
-#error "This library only supports boards with an AVR processor."
-#endif
-
 #include <avr/io.h>
 #include <stdio.h>
 
@@ -17,12 +13,79 @@ extern "C" {
 #define SPI_SCK PB5       // D13 - SCK
 
 /**
+ * @brief Initialize the SPI interface.
+ *
+ * @param pSPIClockSpeed IN -- The clock speed of the SPI interface, in Hz.
  */
-void avr_spi_begin(uint8_t pShieldCSPin, uint32_t pSPIClockSpeed);
+void avr_spi_begin(uint32_t pSPIClockSpeed);
 
 /**
+ * @brief Transfer a byte over SPI.
+ *
+ * @param pByte IN -- The byte to be sent.
+ * @return The byte received.
  */
 uint8_t avr_spi_transfer(uint8_t pByte);
+
+/**
+ * @brief Drive the CS pin low.
+ *
+ */
+void avr_CSLow(void);
+
+/**
+ * @brief Drive the CS pin high.
+ *
+ */
+void avr_CSHigh(void);
+
+/**
+ * @brief Setup function for the AVR platform.
+ *
+ * @param pShieldCSPin IN -- Shield Chip Select pin descriptor or code.
+ * @param pShieldResetPin IN -- Shield reset pin descriptor or code.
+ * @param pSPIClockSpeed IN -- The clock speed of the SPI interface, in Hz.
+ */
+void avr_setup(uint8_t pShieldCSPin, uint8_t pShieldResetPin, uint32_t pSPIClockSpeed);
+
+/**
+ * @brief Transfer a byte over SPI.
+ *
+ * @param pByte IN -- The byte to be sent.
+ * @return The byte received.
+ */
+uint8_t avr_spi_transfer(uint8_t pByte);
+
+/**
+ * @brief Delay function for the AVR platform.
+ *
+ * @param pDelay_us IN -- Delay in microseconds.
+ */
+void avr_delayMicroseconds(uint64_t pDelay_us);
+
+/**
+ * @brief Reset function for the AVR platform.
+ *
+ */
+void avr_reset(void);
+
+/**
+ * @brief Read function for the AVR platform.
+ *
+ * @param pRXBuffer OUT -- Receive buffer.
+ * @param pBufferSize IN -- Size of the buffer in bytes.
+ * @return uint8_t Number of bytes received.
+ */
+uint8_t avr_SPIRead(uint8_t *pRXBuffer, uint8_t pBufferSize);
+
+/**
+ * @brief Write function for the AVR platform.
+ *
+ * @param pTXBuffer IN -- Transmit buffer.
+ * @param pBufferSize IN -- Size of the buffer in bytes.
+ * @return uint8_t Number of bytes written.
+ */
+uint8_t avr_SPIWrite(uint8_t *pTXBuffer, uint8_t pBufferSize);
 
 #ifdef __cplusplus
 }
