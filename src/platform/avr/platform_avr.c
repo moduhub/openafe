dcf89b0595353e15719dc28a9c4d4aca9dba370f
Eduardo-Paz-Putti <eduardopazputti@gmail.com>
2025-08-20 09:33:00 -0300

    [add] Implemented necessary functions for the AVR

--- src/platform/avr/platform_avr.c
+++ src/platform/avr/platform_avr.c
@@ -2,27 +2,49 @@
 extern "C" {
 #endif // __cplusplus
 
-#ifndef ARDUINO_ARCH_AVR
-#error "This library only supports boards with an AVR processor."
+#ifndef F_CPU
+#define F_CPU 16000000UL
 #endif
 
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
 #include "platform_avr.h"
 
-void avr_spi_begin(uint8_t pShieldCSPin, uint32_t pSPIClockSpeed) {
+void avr_spi_begin(uint32_t pSPIClockSpeed) {
 	/**
-	 * If your Arduino board supports SPI speeds higher
+	 * If your avr board supports SPI speeds higher
 	 * than 1 MHz you can go for those speeds, but AVOID
 	 * using SPI speeds lower than 1 MHz, speeds lower
 	 * than that might compromise the sequencer operation.
 	 */
 	(void)pSPIClockSpeed; // Intentionally left unused
-	(void)pShieldCSPin;	  // Intentionally left unused
-
+	
 	// Set MOSI, SCK, and SS as output pins
 	SPI_PORT_DDR |= (1 << SPI_MOSI) | (1 << SPI_SCK) | (1 << SPI_SS);
 
 	// Enable SPI, Master mode, and set clock rate to fck/16
 	SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+
+  // Set CS
+  DDRD |= (1 << SPI_SS);
+}
+
+void avr_CSLow(void){	 
+	PORTD &= ~(1 << SPI_SS);
+}
+
+void avr_CSHigh(void){
+	PORTD |= (1 << SPI_SS);
+}
+
+void avr_setup(uint8_t pShieldCSPin, uint8_t pShieldResetPin, uint32_t pSPIClockSpeed){
+  (void)pShieldResetPin; // Intentionally left unused
+  (void)pShieldCSPin;
+  avr_spi_begin(pSPIClockSpeed);
+  DDRD |= (1 << 3);	 
+  PORTD &= ~(1 << 3);
+  avr_delayMicroseconds(100);
 }
 
 uint8_t avr_spi_transfer(uint8_t pByte){
@@ -35,6 +57,35 @@ uint8_t avr_spi_transfer(uint8_t pByte){
 
 	// Return received byte
 	return SPDR;
+}
+
+void avr_delayMicroseconds(uint64_t pDelay_us){
+	if (pDelay_us > 10000u)
+		_delay_ms(pDelay_us / 1000);
+	else
+		_delay_us(pDelay_us);
+}
+
+void avr_reset(void){
+	PORTD |= (1 << 3);
+	avr_delayMicroseconds(1000); 
+	PORTD &= ~(1 << 3);
+}
+
+uint8_t avr_SPIRead(uint8_t *pRXBuffer, uint8_t pBufferSize){
+	// Read process here ...
+	// Return the amount bytes read
+	(void)pRXBuffer;   // Intentionally left unused
+	(void)pBufferSize; // Intentionally left unused
+	return 0;
+}
+
+uint8_t avr_SPIWrite(uint8_t *pTXBuffer, uint8_t pBufferSize){
+	// Write process here ...
+	// Return the amount of bytes written
+	(void)pTXBuffer;   // Intentionally left unused
+	(void)pBufferSize; // Intentionally left unused
+	return 0;
 }
 
 #ifdef __cplusplus
