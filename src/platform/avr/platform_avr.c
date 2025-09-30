eec352ab19f7e68eef62ee1683fc37629e5596cf
Eduardo-Paz-Putti <eduardopazputti@gmail.com>
2025-09-30 14:24:14 -0300

    [add] implemented bit bang for low-frequency SPI (f_osc<125kHz)

--- src/platform/avr/platform_avr.c
+++ src/platform/avr/platform_avr.c
@@ -14,6 +14,9 @@ extern "C" {
 
 #include "platform_avr.h"
 
+static uint8_t spi_use_bitbang = 0;     
+static uint32_t spi_bitbang_delay_us = 0; // µs (T/2)
+
 void avr_digitalWrite(uint8_t pin, uint8_t val) {
   volatile uint8_t *out = 0;
   uint8_t bit;
@@ -48,43 +51,127 @@ void avr_setup(uint8_t pShieldCSPin, uint8_t pShieldResetPin, uint32_t pSPIClock
   avr_digitalWrite(3, 0);
 }
 
-void avr_spi_begin(uint32_t pSPIClockSpeed) {
-	/**
+/**
 	 * If your avr board supports SPI speeds higher
 	 * than 1 MHz you can go for those speeds, but AVOID
 	 * using SPI speeds lower than 1 MHz, speeds lower
 	 * than that might compromise the sequencer operation.
+   * para o LPDAC com WAVEGEN é necessári oque tenha um SPi com menos de 32Khz/16
 	 */
-	(void)pSPIClockSpeed; // Intentionally left unused
-	
-	// Set MOSI, SCK, and SS as output pins
-	SPI_PORT_DDR |= (1 << SPI_MOSI) | (1 << SPI_SCK) | (1 << SPI_SS);
+void avr_spi_begin(uint32_t pSPIClockSpeed) {
+  // Configure DDRs: MOSI, SCK, SS outputs; MISO input
+  SPI_PORT_DDR |= (1 << SPI_MOSI) | (1 << SPI_SCK) | (1 << SPI_SS);
+  SPI_PORT_DDR &= ~(1 << SPI_MISO);
 
-	// Enable SPI, Master mode, and set clock rate to fck/16
-	SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+  // Ensure outputs default states: MOSI=0, SCK=0, SS=HIGH (idle)
+  SPI_PORT &= ~((1 << SPI_MOSI) | (1 << SPI_SCK));
+  // IMPORTANT: disable pull-up on MISO (clear PORT bit) so input floats low when tri-state
+  SPI_PORT &= ~(1 << SPI_MISO);
+  SPI_PORT |= (1 << SPI_SS);
 
-  // Set CS
-  DDRD |= (1 << SPI_SS);
+  // Clear SPI control registers first (disable hardware SPI)
+  SPCR = 0;
+  SPSR = 0;
+
+  spi_use_bitbang = 0;
+
+  // fosc/128 (125kHz com 16MHz), ative bitbang
+  if (pSPIClockSpeed < (F_CPU / 128UL)) {
+    SPCR &= ~(1 << SPE); 
+
+    spi_use_bitbang = 1;
+    // calculate T/2
+    float T_us = (1.0f / (float)pSPIClockSpeed) * 1000000.0f;
+    uint32_t half = (uint32_t)(T_us / 2.0f);
+    if (half == 0) half = 1; 
+    spi_bitbang_delay_us = half;
+
+    SPI_PORT &= ~(1 << SPI_SCK);
+    SPI_PORT &= ~(1 << SPI_MOSI);
+    SPI_PORT |= (1 << SPI_SS);
+    return;
+  }
+
+  SPCR |= (1 << SPE) | (1 << MSTR);
+  SPCR &= ~((1 << SPR1) | (1 << SPR0));
+  SPSR &= ~(1 << SPI2X);
+
+  if (pSPIClockSpeed >= (F_CPU / 2UL)) {
+    SPSR |= (1 << SPI2X);
+    // SPCR SPRx = 0 => fosc/2 when SPI2X=1
+  } else if (pSPIClockSpeed >= (F_CPU / 4UL)) {
+    // fosc/4 (default)
+  } else if (pSPIClockSpeed >= (F_CPU / 8UL)) {
+    SPSR |= (1 << SPI2X);
+    SPCR |= (1 << SPR0); // fosc/8
+  } else if (pSPIClockSpeed >= (F_CPU / 16UL)) {
+    SPCR |= (1 << SPR0); // fosc/16
+  } else if (pSPIClockSpeed >= (F_CPU / 32UL)) {
+    SPSR |= (1 << SPI2X);
+    SPCR |= (1 << SPR1); // fosc/32
+  } else if (pSPIClockSpeed >= (F_CPU / 64UL)) {
+    SPCR |= (1 << SPR1); // fosc/64
+  } else {
+    SPCR |= (1 << SPR1) | (1 << SPR0); // fosc/128
+  }
+
+  // ensure SCK idle low e CS alto
+  SPI_PORT &= ~(1 << SPI_SCK);
+  SPI_PORT |= (1 << SPI_SS);
 }
 
-uint8_t avr_spi_transfer(uint8_t pByte){
-	// Start transmission
-	SPDR = pByte;
+uint8_t avr_spi_transfer(uint8_t pByte) {
+  if (!spi_use_bitbang) {
+    // Hardware SPI
+    SPDR = pByte;
+    while (!(SPSR & (1 << SPIF)));
+    return SPDR;
+  } else {
+    // Bit-bang SPI: CPOL=0, CPHA=0, MSB first
+    uint8_t received = 0;
+    uint8_t oldSREG = SREG;
+    cli();
 
-	// Wait for transmission to complete
-	while (!(SPSR & (1 << SPIF)))
-		;
+    SPI_PORT &= ~(1 << SPI_SCK);
 
-	// Return received byte
-	return SPDR;
+    for (int8_t i = 7; i >= 0; --i) {
+      // MOSI
+      if (pByte & (1 << i)) SPI_PORT |= (1 << SPI_MOSI);
+      else SPI_PORT &= ~(1 << SPI_MOSI);
+
+      // clock ↑
+      SPI_PORT |= (1 << SPI_SCK);
+      avr_delayMicroseconds(spi_bitbang_delay_us);
+
+      // MISO 
+      if (PINB & (1 << SPI_MISO))
+        received |= (1 << i);
+
+      // clock ↓
+      SPI_PORT &= ~(1 << SPI_SCK);
+      avr_delayMicroseconds(spi_bitbang_delay_us);
+    }
+
+    SREG = oldSREG;
+    return received;
+  }
 }
 
+/**
+ * Delay in microseconds that accepts variable values
+ */
 void avr_delayMicroseconds(uint64_t pDelay_us){
-	if (pDelay_us > 10000u)
-		_delay_ms(pDelay_us / 1000);
-	else
-		_delay_us(pDelay_us);
+  // _delay_ms(1) e _delay_us(1) aceitam argumentos constantes, usamos loops
+  while (pDelay_us >= 1000ull) {
+    _delay_ms(1);
+    pDelay_us -= 1000ull;
+  }
+  /* agora <1000 us */
+  while (pDelay_us--) {
+    _delay_us(1); // 1 é constante -> compilador aceita
+  }
 }
+
 
 void avr_reset(void){
   avr_digitalWrite(3, 1);
