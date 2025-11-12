311cbfe1ad15f0fda935d57d0379ec7e67896633
Eduardo-Paz-Putti <eduardopazputti@gmail.com>
2025-11-12 09:27:46 -0300

    [feature] Creation and setup of an example for EIS .ino

--- examples/EISSequencer/EISSequencer.ino
+++ examples/EISSequencer/EISSequencer.ino
@@ -5,7 +5,44 @@ void setup(){
 	
 	AFE openAFE;
 
-  openAFE.setEISSinSequence();
+  pinMode(2, INPUT);
+	noInterrupts();
+	//attachInterrupt(digitalPinToInterrupt(2), openAFE.interruptHandler_EIS, LOW);
+  // pinMode(2, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), openAFE.interruptHandler_EIS,FALLING);   
+	delay(200);
+
+  int success;
+  int settlingTime = 1000;
+  int startingOmega = 1000;
+  int endingOmega = 10000;
+  int stepForADecade = 10;
+  int samplesPerFrequency = 10;
+
+  success = openAFE.setEISSequence(settlingTime, startingOmega, endingOmega, stepForADecade, samplesPerFrequency);
+
+  if (success){
+    Serial.println(F("EIS iniciado")); 
+    openAFE.startEIS();
+    interrupts();
+		
+		do {
+			if (openAFE.dataAvailable_EIS() > 0){
+				noInterrupts();
+              
+        Serial.println(openAFE.dataAvailable_EIS());
+        openAFE.getPoint_EIS();
+
+        interrupts();
+			}
+			delay(1);
+		} while (true);
+
+		Serial.println(F("<<< FINISHED EIS >>>")); 
+  }
+  else Serial.println(F("*** ERROR: Cannot generate desired waveform! ***"));
+
+  while (true);
 }
 
 void loop(){}
