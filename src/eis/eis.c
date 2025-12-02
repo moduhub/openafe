5dfa1589e2171f2bff64cd1ead713a307ac80345
Eduardo-Paz-Putti <eduardopazputti@gmail.com>
2025-12-02 14:11:48 -0300

    [misc] Fixed formatting of ADC configuration comment in AD5941_setupADC_for_EIS

--- src/eis/eis.c
+++ src/eis/eis.c
@@ -345,7 +345,7 @@ void AD5941_waveOFF(void){
 // ADC GENERAL CONFIG.
 void AD5941_setupADC_for_EIS(void){
   uint32_t adccon = 0UL
-    //| (0b11UL << 16)  // GNPGA = 11 -> PGA gain = 4 (to compensate for the HSDAC not having the gain 1 option)
+    | (0b11UL << 16)  // GNPGA = 11 -> PGA gain = 4 (to compensate for the HSDAC not having the gain 1 option)
     //| (1UL << 15)     // ?? Enables dc offset cancellation
     | (0b00001 << 8)  // (MUXSELN negative input) High speed TIA negative input
     | (0b00001);      // (MUXSELN positive input) High speed TIA positive signal.
